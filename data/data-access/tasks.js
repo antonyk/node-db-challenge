6d218abb773c6d115bbc99276648dde2cc5f3de6
Anthony K <antonyk@users.noreply.github.com>
2020-05-15 14:19:32 -0400

    improved DA methods for deep get

--- data/data-access/tasks.js
+++ data/data-access/tasks.js
@@ -2,25 +2,51 @@ const db = require('../dbConnection');
 module.exports = {
   all,
   create,
-  getOne
+  getOne,
+  getSetDeep,
+  getSet
 }
 
 const tableName = 'tasks';
 
-function all(id) {
-  return id ?
-  db(tableName).where('id', id) : 
-  db(tableName);
+function getSet() {
+  return db(tableName);
+}
+
+function getSetDeep() {
+  return db(tableName)
+    .join('projects', 'tasks.project_id', 'projects.id')
+    .select('*');
 }
 
 function getOne(id) {
   if (id) {
-    return all(id).first();
-  }
-  else {
+    return getSet()
+      .where('tasks.id', id)
+      .first()
+  } else {
     throw 'must pass an id argument';
   }
 }
+
+function getOneDeep(id) {
+  if (id) {
+    return getSetDeep()
+      .where('tasks.id', id)
+      .first()
+  } else {
+    throw 'must pass an id argument';
+  }
+}
+
+function all(id, deep = true) {
+  if (id) {
+    return deep ? getOneDeep(id) : getOne(id);
+  } else {
+    return deep ? getSetDeep(id) : getSet(id);
+  }
+}
+
 
 function create(data) {
   return db(tableName)
@@ -28,7 +54,7 @@ function create(data) {
     .then(result => {
       console.log('task insert res:', result)
       if (result)
-        return getOne(result[0]);
+        return getOneDeep(result[0]);
       else {
         throw 'database failed to return new ID';
       }
