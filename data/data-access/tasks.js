f197ac6185e52853dad34f891f1b306dae84d64b
Anthony K <antonyk@users.noreply.github.com>
2020-05-15 13:44:05 -0400

    implemented data access and endpoints for tasks

---
+++ data/data-access/tasks.js
@@ -0,0 +1,36 @@
+const db = require('../dbConnection');
+module.exports = {
+  all,
+  create,
+  getOne
+}
+
+const tableName = 'tasks';
+
+function all(id) {
+  return id ?
+  db(tableName).where('id', id) : 
+  db(tableName);
+}
+
+function getOne(id) {
+  if (id) {
+    return all(id).first();
+  }
+  else {
+    throw 'must pass an id argument';
+  }
+}
+
+function create(data) {
+  return db(tableName)
+    .insert(data, 'id')
+    .then(result => {
+      console.log('task insert res:', result)
+      if (result)
+        return getOne(result[0]);
+      else {
+        throw 'database failed to return new ID';
+      }
+    })
+}
